d190561d362f8c239c704b5db1b77ec36e5ad703
Kelvin Modesto <km.modesto@gmail.com>
2020-08-31 23:56:14 -0300

    :rotating_light: (lint)
    Config lint to airbnb style guide

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,36 @@
 module.exports = {
-    "env": {
-        "es2020": true,
-        "node": true
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+    'plugin:import/typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 11,
+    sourceType: 'module',
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'import/extensions': ['error', 'ignorePackages', { ts: 'never' }],
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'consistent-return': 'off',
+    'no-console': ['error', { allow: ['error'] }],
+    'no-bitwise': 'off',
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        project: './src',
+      },
     },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 12,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+  },
 };
