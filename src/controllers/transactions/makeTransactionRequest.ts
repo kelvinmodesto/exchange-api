dbc5ad5be15a4c0533be63388241e307f77227a9
Kelvin Modesto <km.modesto@gmail.com>
2020-09-07 16:16:54 -0300

    :recycle: (http post method) finishing makeTransactionRequest

--- src/controllers/transactions/makeTransactionRequest.ts
+++ src/controllers/transactions/makeTransactionRequest.ts
@@ -4,20 +4,60 @@ import { ContextStrategy } from '../../db';
 
 const makeTransactionRequest = (router: Router, context: ContextStrategy) => {
   const monetaryUnitList = ['GBP', 'BRL', 'JPY', 'EUR', 'USD'];
-  const { log } = console;
   router.post('/', async (req: any, res: any, next: any) => {
     try {
-      const { userId, monetaryUnitSrc, monetaryUnitDest, valueSrc } = req.body;
-      const conversionRates = await getRates(monetaryUnitSrc);
-      log(conversionRates);
-      // if (res.status === 200) {
-      // if (!userId) return new Error('Request has no given userId');
-      // if (makeTransaction()) {
-      // }
-      // } else {
-      //   return new Error('Request Error');
-      // }
-      next('Bádi');
+      const { userId, monetaryUnitSrc, monetaryUnitDest, srcValue } = req.query;
+      const conversionRate = await getRates(monetaryUnitSrc, monetaryUnitDest);
+      if (res.statusCode === 200) {
+        if (!userId) throw new Error('Request has no given userId');
+        if (!srcValue) throw new Error('Request has no given value to convert');
+        if (monetaryUnitList.indexOf(monetaryUnitSrc) === -1) {
+          throw new Error('Request source monetary unit is not valid');
+        }
+        if (monetaryUnitList.indexOf(monetaryUnitDest) === -1) {
+          throw new Error('Request destiny monetary unit is not valid');
+        }
+        const {
+          dataValues: { transactionId, createdAt },
+        } = await context.create({
+          userId,
+          conversionRate,
+          srcValue,
+          monetaryUnitSrc,
+          monetaryUnitDest,
+        });
+        if (makeTransaction()) {
+          const destValue = (srcValue * conversionRate).toFixed(2);
+          await context.update(
+            {
+              destValue,
+              transactionIsSucceeded: true,
+            },
+            { where: { transactionId } }
+          );
+          res.send({
+            transactionId,
+            userId,
+            createdAt,
+            conversionRate,
+            srcValue,
+            monetaryUnitSrc,
+            monetaryUnitDest,
+            destValue,
+          });
+        } else {
+          res.send({
+            userId,
+            createdAt,
+            conversionRate,
+            srcValue,
+            monetaryUnitSrc,
+            monetaryUnitDest,
+          });
+        }
+      } else {
+        throw new Error('Request Error');
+      }
     } catch (error) {
       next(error);
     }
